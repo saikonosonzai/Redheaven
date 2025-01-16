817efea965b1cd84bd88e40d6731cab290068679
Noah <90843368+saikonosonzai@users.noreply.github.com>
2025-01-16 03:47:59 +0100

    github macht faxen

--- pages/anmelden/anmelden.js
+++ pages/anmelden/anmelden.js
@@ -1,4 +1,4 @@
-addEventListener("submit", (event) => {
+document.getElementById("form").addEventListener("submit", (event) => {
     event.preventDefault();
     localStorage.setItem("loggedIn", "true");
     window.location.href = "../../index.html";

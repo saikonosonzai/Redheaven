bfcf83d13958cb06188a0f24ec0d5e5d55a10dd3
SaikoEnigma <166615287+SaikoEnigma@users.noreply.github.com>
2025-01-19 09:23:41 +0100

    Connection von Produkten zur 1. Wohnung und ein extra Mesh für den Mars wurde hinzugefügt.

--- pages/produkt/produkt.js
+++ pages/produkt/produkt.js
@@ -5,24 +5,24 @@ import { GLTFLoader } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/l
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
+//setzt die Kamera Position
 camera.position.x = 300;
-camera.position.z = 300;
 
 
-//Keep the 3D object on a global variable so we can access it later
 let object;
 
-//OrbitControls allow the camera to move around the scene
+//Zum Bewegen
 let controls;
 
 
-//Set which object to render
+//Welches Model dargestellt werden soll
 let objToRender = 'mars';
+let objToRender_extra = 'mars_extra';
 
-//Instantiate a loader for the .gltf file
+//Ein Loader für GLFT Files
 const loader = new GLTFLoader();
 
-//Load the file
+//Datei laden
 loader.load(
     `../../resources/models/${objToRender}/scene.gltf`,
     function (gltf) {
@@ -40,17 +40,32 @@ loader.load(
     }
 );
 
-//Instantiate a new renderer and set its size
+loader.load(
+    `../../resources/models/${objToRender_extra}/scene.gltf`,
+    function (gltf) {
+        //If the file is loaded, add it to the scene
+        object = gltf.scene;
+        scene.add(object);
+    },
+    function (xhr) {
+        //While it is loading, log the progress
+        console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+    },
+    function (error) {
+        //If there is an error, log it
+        console.error(error);
+    }
+);
+
+//Neuen Renderer instanzieren
 const renderer = new THREE.WebGLRenderer({ alpha: true }); //Alpha: true allows for the transparent background
 renderer.setSize(window.innerWidth, window.innerHeight);
 
-//Add the renderer to the DOM
+//Adde den Renderer
 document.getElementById("container3D").appendChild(renderer.domElement);
 
-//Set how far the camera will be from the 3D model
-camera.position.z = objToRender === "mars" ? 50 : 500;
 
-//Add lights to the scene, so we can actually see the 3D model
+//Added Lichter zu der Scene
 const topLight = new THREE.DirectionalLight(0xffffff, 1); // (color, intensity)
 topLight.position.set(500, 500, 500) //top-left-ish
 topLight.castShadow = true;
@@ -59,29 +74,25 @@ scene.add(topLight);
 const ambientLight = new THREE.AmbientLight(0x333333, objToRender === "mars" ? 5 : 1);
 scene.add(ambientLight);
 
-//This adds controls to the camera, so we can rotate / zoom it with the mouse
-if (objToRender === "mars") {
-    controls = new OrbitControls(camera, renderer.domElement);
-}
+//zum Bewegen des Objektes
+controls = new OrbitControls(camera, renderer.domElement);
 
-//Render the scene
+//Rendern
 function animate() {
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
 }
 
-//Add a listener to the window, so we can resize the window and the camera
+//Listener damit der Renderer mit zoomt
 window.addEventListener("resize", function () {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
     renderer.setSize(window.innerWidth, window.innerHeight);
 });
 
-
-if (objToRender === "mars") {
     controls.enableRotate = true;
     controls.enableZoom = false;
     controls.enablePan = false;
-}
-//Start the 3D rendering
+
+//startet das rendering
 animate();
--- pages/produkt/wohnungen/wohnungen.js
+++ pages/produkt/wohnungen/wohnungen.js
@@ -5,9 +5,8 @@ import { GLTFLoader } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/l
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
+//Kamera Position
 camera.position.x = 300;
-camera.position.z = 300;
-
 
 let object;
 
@@ -39,17 +38,17 @@ loader.load(
     }
 );
 
-//Instantiate a new renderer and set its size
+//Neuen Renderer instanzieren
 const renderer = new THREE.WebGLRenderer({ alpha: true }); //Alpha: true allows for the transparent background
 renderer.setSize(window.innerWidth, window.innerHeight);
 
-//Add the renderer to the DOM
+//Adde den Renderer
 document.getElementById("container3D").appendChild(renderer.domElement);
 
 //Set how far the camera will be from the 3D model
-camera.position.z = objToRender === "wohnung1" ? 50 : 500;
 
-//Add lights to the scene, so we can actually see the 3D model
+
+//Added Lichter zu der Scene
 const topLight = new THREE.DirectionalLight(0xffffff, 1); // (color, intensity)
 topLight.position.set(500, 500, 500) //top-left-ish
 topLight.castShadow = true;
@@ -58,10 +57,8 @@ scene.add(topLight);
 const ambientLight = new THREE.AmbientLight(0x333333, objToRender === "wohnung1" ? 5 : 1);
 scene.add(ambientLight);
 
-//This adds controls to the camera, so we can rotate / zoom it with the mouse
-if (objToRender === "wohnung1") {
-    controls = new OrbitControls(camera, renderer.domElement);
-}
+//zum Bewegen und zoomen des Objektes
+controls = new OrbitControls(camera, renderer.domElement);
 
 //Rendern
 function animate() {
@@ -69,7 +66,7 @@ function animate() {
     renderer.render(scene, camera);
 }
 
-//Add a listener to the window, so we can resize the window and the camera
+//Listener damit der Renderer mit zoomt
 window.addEventListener("resize", function () {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
@@ -81,5 +78,5 @@ window.addEventListener("resize", function () {
     controls.enablePan = false;
 
 
-//Start the 3D rendering
+//startet das rendering
 animate();

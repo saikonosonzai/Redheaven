1fa5077632157f7bb7f4105d32e314a4ee41a278
SaikoEnigma <166615287+SaikoEnigma@users.noreply.github.com>
2025-01-19 19:04:14 +0100

    Design von Wohnung verbessert

--- pages/produkt/wohnungen/wohnungen.js
+++ pages/produkt/wohnungen/wohnungen.js
@@ -1,6 +1,6 @@
 import * as THREE from "https://cdn.skypack.dev/three@0.129.0/build/three.module.js";
-import { OrbitControls } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/controls/OrbitControls.js";
-import { GLTFLoader } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js";
+import {OrbitControls} from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/controls/OrbitControls.js";
+import {GLTFLoader} from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js";
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -39,7 +39,7 @@ loader.load(
 );
 
 //Neuen Renderer instanzieren
-const renderer = new THREE.WebGLRenderer({ alpha: true }); //Alpha: true allows for the transparent background
+const renderer = new THREE.WebGLRenderer({alpha: true}); //Alpha: true allows for the transparent background
 renderer.setSize(window.innerWidth, window.innerHeight);
 
 //Adde den Renderer
@@ -73,9 +73,9 @@ window.addEventListener("resize", function () {
     renderer.setSize(window.innerWidth, window.innerHeight);
 });
 
-    controls.enableRotate = true;
-    controls.enableZoom = true;
-    controls.enablePan = false;
+controls.enableRotate = true;
+controls.enableZoom = true;
+controls.enablePan = false;
 
 
 //startet das rendering
